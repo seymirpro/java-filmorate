bb19d1ec857da00910886fa79e1d714326c4d504
sayvazov <seymirpro@yandex.ru>
2023-05-30 18:03:29 +0300

    refact - upd in filmtest using builder

--- src/test/java/ru/yandex/practicum/filmorate/model/FilmTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/model/FilmTest.java
@@ -1,10 +1,15 @@
 package ru.yandex.practicum.filmorate.model;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
-import javax.validation.Validation;
-import javax.validation.Validator;
-import javax.validation.ValidatorFactory;
+import javax.validation.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 class FilmTest {
 
@@ -16,52 +21,70 @@ class FilmTest {
         validator = factory.getValidator();
     }
 
-    /*@Test
+    @Test
     public void shouldContainNonEmptyName() {
-        @Valid Film film = new Film(1, "", "description",
-                1, LocalDate.now(), 20, LocalDateTime.now());
+        @Valid Film film = Film.builder()
+                .id(1)
+                .name("")
+                .description("description")
+                .releaseDate(LocalDate.now())
+                .duration(20)
+                .createdAt(LocalDateTime.now())
+                .build();
         Set<ConstraintViolation<Film>> violations = validator.validate(film);
         assertFalse(violations.isEmpty());
-    }*/
+    }
 
-   /* @Test
+    @Test
     public void shouldContainDescriptionLengthLessOrEqual200() {
-        @Valid Film film = new Film(1, "The gentlemen", "descriptiondescriptiondescript" +
-                "iondescriptiondescriptionde" +
-                "scriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescripti" +
-                "ondescriptiondescript" +
-                "iondescriptiondescriptiondescriptiondescriptiondescriptiondescriptio" +
-                "ndescriptiondescript" +
-                "iondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescription" +
-                "descriptiondescription" +
-                "descriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescr" +
-                "iptiondescriptiondescriptiondescriptiondescriptiondescriptionde" +
-                "scriptiondescriptiondescription", 1, LocalDate.now(), 20, LocalDateTime.now());
+        @Valid Film film = Film.builder()
+                .id(1)
+                .name("The gentlemen")
+                .description("descriptiondescriptiondescript" +
+                        "iondescriptiondescriptionde" +
+                        "scriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescripti" +
+                        "ondescriptiondescript" +
+                        "iondescriptiondescriptiondescriptiondescriptiondescriptiondescriptio" +
+                        "ndescriptiondescript" +
+                        "iondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescription" +
+                        "descriptiondescription" +
+                        "descriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescr" +
+                        "iptiondescriptiondescriptiondescriptiondescriptiondescriptionde" +
+                        "scriptiondescriptiondescription")
+                .releaseDate(LocalDate.now())
+                .createdAt(LocalDateTime.now())
+                .duration(2)
+                .build();
         Set<ConstraintViolation<Film>> violations = validator.validate(film);
         violations.stream().forEach(System.out::println);
         assertFalse(violations.isEmpty());
     }
 
-    */
-
-    /*@Test
+    @Test
     public void shouldNotHaveReleaseDateBefore18951228() {
-        @Valid Film film = new Film(1, "The Gentlemen", "description", 2,
-                LocalDate.of(1700, 1, 1),
-                20, LocalDateTime.now());
+        @Valid Film film = Film.builder()
+                .id(1)
+                .name("The Gentlemen")
+                .description("description")
+                .releaseDate(LocalDate.of(1700, 1, 1))
+                .duration(2)
+                .createdAt(LocalDateTime.now())
+                .build();
         Set<ConstraintViolation<Film>> violations = validator.validate(film);
         assertFalse(violations.isEmpty());
     }
 
-     */
-
-    /*@Test
+    @Test
     public void shouldHavePositiveDuration() {
-        @Valid Film film = new Film(1, "The Gentlemen", "description", 2,
-                LocalDate.of(1900, 1, 1), -19, LocalDateTime.now());
+        @Valid Film film = Film.builder()
+                .id(1)
+                .name("The Gentlemen")
+                .releaseDate(LocalDate.of(1900, 1, 1))
+                .mpa(RatingMpa.builder().build())
+                .createdAt(LocalDateTime.now())
+                .genres(new ArrayList<Genre>())
+                .build();
         Set<ConstraintViolation<Film>> violations = validator.validate(film);
         assertFalse(violations.isEmpty());
     }
-
-     */
 }
