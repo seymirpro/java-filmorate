1cc0cb5b6605b505a4173c68df6bf3873600c06a
sayvazov <seymirpro@yandex.ru>
2023-05-30 19:21:15 +0300

    refact - upd in codestyle

--- src/main/java/ru/yandex/practicum/filmorate/utils/validation/constraints/model_validators/UserValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/utils/validation/constraints/model_validators/UserValidator.java
@@ -5,7 +5,7 @@ import ru.yandex.practicum.filmorate.model.User;
 import java.time.LocalDate;
 
 public class UserValidator {
-    public static boolean validate(User user){
+    public static boolean validate(User user) {
         return user.getLogin().contains(" ") ||
                 user.getBirthday().isAfter(LocalDate.now());
     }
